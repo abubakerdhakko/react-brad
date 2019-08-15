564172da6b20ae5b529870a488f6c52edcd296b8
abubakerdhakko <abubakerdhakko9212@gmail.com>
2019-08-15 04:36:15 -0700

    Updates

---
+++ precache-manifest.b24aff82e56a0591f4a651a76631eab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b6a3430b0f19534ad51",
+    "url": "/react-brad/static/js/runtime~main.bd5c20c1.js"
+  },
+  {
+    "revision": "29be9fe481e0df24fc54",
+    "url": "/react-brad/static/js/main.661754b7.chunk.js"
+  },
+  {
+    "revision": "fd8efeae3c6298aee976",
+    "url": "/react-brad/static/js/2.4d11f7a7.chunk.js"
+  },
+  {
+    "revision": "29be9fe481e0df24fc54",
+    "url": "/react-brad/static/css/main.91f36700.chunk.css"
+  },
+  {
+    "revision": "fd8efeae3c6298aee976",
+    "url": "/react-brad/static/css/2.aff52eb7.chunk.css"
+  },
+  {
+    "revision": "a6a070e2ac247d16c7b3c9a0327d24fd",
+    "url": "/react-brad/index.html"
+  }
+];
